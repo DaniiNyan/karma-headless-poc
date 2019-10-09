3538a912605a9b619fb121105bdf1bf696501da0
DaniiNyan <daniela.araujo@ilegra.com>
2019-10-09 19:21:05 -0300

    Correct bug, install karma-electron-launcher

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-electron'),
+      require('karma-electron-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -27,9 +27,6 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Electron'],
-    preprocessors: {
-      '**/*.js': ['electron']
-    },
     singleRun: false,
     restartOnFileChange: true
   });
