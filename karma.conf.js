99ebff5237f3b9c963a753e8693ee84a9cd38f22
DaniiNyan <daniela.araujo@ilegra.com>
2019-10-10 12:16:28 -0300

    Test ChromeHeadless

--- karma.conf.js
+++ karma.conf.js
@@ -1,13 +1,15 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
 module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-electron'),
+      require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -25,9 +27,9 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: false,
-    browsers: ['Electron'],
-    singleRun: true,
+    autoWatch: true,
+    browsers: ['ChromeHeadless'],
+    singleRun: false,
     restartOnFileChange: true
   });
 };
