ec88a878ed279306796df4c6386325175c081702
DaniiNyan <daniela.araujo@ilegra.com>
2019-10-10 12:07:25 -0300

    Add karma-electron and set singleRun as true

--- karma.conf.js
+++ karma.conf.js
@@ -25,9 +25,9 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
+    autoWatch: false,
     browsers: ['Electron'],
-    singleRun: false,
+    singleRun: true,
     restartOnFileChange: true
   });
 };
